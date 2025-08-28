c2c9e655aee1f89fa74bc493dd23163eaea1b8a1
mojimoon <cryovit@outlook.com>
2025-08-28 17:31:48 +0800

    implement argumented @c @hd @vd @ho @v

--- minesweepervariants/impl/board/dye/c.py
+++ minesweepervariants/impl/board/dye/c.py
@@ -12,10 +12,16 @@ class DyeC(AbstractDye):
     name = "c"
     __doc__ = "棋盘格染色"
 
+    def __init__(self, args):
+        if args:
+            self.offset = 1
+        else:
+            self.offset = 0
+
     def dye(self, board):
         dye = True
         for key in board.get_interactive_keys():
             dye = not dye
             for pos, _ in board(key=key):
-                _dye = dye ^ ((pos.x + pos.y) % 2 > 0)
+                _dye = dye ^ ((pos.x + pos.y + self.offset) % 2 > 0)
                 board.set_dyed(pos, _dye)
--- minesweepervariants/impl/board/dye/v.py
+++ minesweepervariants/impl/board/dye/v.py
@@ -5,10 +5,16 @@ class DyeC(AbstractDye):
     name = "v"
     __doc__ = "竖直染色"
 
+    def __init__(self, args):
+        if args:
+            self.offset = 1
+        else:
+            self.offset = 0
+
     def dye(self, board):
         dye = True
         for key in board.get_interactive_keys():
             dye = not dye
             for pos, _ in board(key=key):
-                _dye = dye ^ (pos.y % 2 > 0)
+                _dye = dye ^ ((pos.y + self.offset) % 2 > 0)
                 board.set_dyed(pos, _dye)
