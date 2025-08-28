ccb83f4ebec7640d54817fb0b2bc703eca4bc11c
mojimoon <cryovit@outlook.com>
2025-08-28 18:28:05 +0800

    implement arugmented @sp

--- minesweepervariants/impl/board/dye/sp.py
+++ minesweepervariants/impl/board/dye/sp.py
@@ -5,10 +5,18 @@ class DyeSP(AbstractDye):
     __doc__ = "螺旋染色"
 
     def __init__(self, args):
-        if args:
-            self.base_color = True
-        else:
+        # if args:
+        #     self.base_color = True
+        # else:
+        #     self.base_color = False # 背景色是非染色（第一圈染色）
+
+        if not args:
             self.base_color = False # 背景色是非染色（第一圈染色）
+            self.clockwise = True # 顺时针
+        else:
+            all_args = args.split(':')
+            self.base_color = all_args[0] != '0'
+            self.clockwise = True if len(all_args) == 1 else all_args[1] == '0'
     
     def dye(self, board):
         dye = self.base_color
@@ -24,18 +32,24 @@ class DyeSP(AbstractDye):
 
             top, bottom, left, right = 0, n - 1, 0, m - 1
             while top <= bottom and left <= right:
-                for j in range(left, right + 1): # (top, left) -> (top, right)
-                    print('PATH 1', top, j)
-                    board.set_dyed(board.get_pos(top, j), dye)
-                for i in range(top + 1, bottom): # (top+1, right) -> (bottom-1, right)
-                    print('PATH 2', i, right)
-                    board.set_dyed(board.get_pos(i, right), dye)
-                for j in range(right - 1, left, -1): # (bottom-1, right-1) -> (bottom-1, left+1)
-                    print('PATH 3', bottom-1, j)
-                    board.set_dyed(board.get_pos(bottom-1, j), dye)
-                for i in range(bottom - 2, top + 1, -1): # (bottom-2, left+1) -> (top, left+1)
-                    print('PATH 4', i, left)
-                    board.set_dyed(board.get_pos(i, left+1), dye)
+                if self.clockwise:
+                    for j in range(left, right + 1): # (top, left) -> (top, right)
+                        board.set_dyed(board.get_pos(top, j), dye)
+                    for i in range(top + 1, bottom): # (top+1, right) -> (bottom-1, right)
+                        board.set_dyed(board.get_pos(i, right), dye)
+                    for j in range(right - 1, left, -1): # (bottom-1, right-1) -> (bottom-1, left+1)
+                        board.set_dyed(board.get_pos(bottom-1, j), dye)
+                    for i in range(bottom - 2, top + 1, -1): # (bottom-2, left+1) -> (top, left+1)
+                        board.set_dyed(board.get_pos(i, left+1), dye)
+                else:
+                    for i in range(top, bottom + 1):
+                        board.set_dyed(board.get_pos(i, left), dye)
+                    for j in range(left + 1, right):
+                        board.set_dyed(board.get_pos(bottom, j), dye)
+                    for i in range(bottom - 1, top, -1):
+                        board.set_dyed(board.get_pos(i, right-1), dye)
+                    for j in range(right - 2, left + 1, -1):
+                        board.set_dyed(board.get_pos(top+1, j), dye)
                 top += 2
                 bottom -= 2
                 left += 2
