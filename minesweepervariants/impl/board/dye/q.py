c1e420a5f9501b75e945133386533feb019897a5
mojimoon <cryovit@outlook.com>
2025-08-28 18:30:27 +0800

    implement argumented @q

--- minesweepervariants/impl/board/dye/q.py
+++ minesweepervariants/impl/board/dye/q.py
@@ -13,22 +13,28 @@ class DyeC(AbstractDye):
     __doc__ = "2x2棋盘格染色"
 
     def __init__(self, args):
-        if args is not None and args.isdigit():
-            self.offset = int(args)
+        if not args:
+            self.size = 2
+            self.dx = 0
+            self.dy = 0
         else:
-            self.offset = 0
+            all_args = args.split(':')
+            self.size = int(all_args[0])
+            self.dx = int(all_args[1]) if len(all_args) > 1 else 0
+            self.dy = int(all_args[2]) if len(all_args) > 2 else 0
 
     def dye(self, board):
         dye = True
         for key in board.get_interactive_keys():
             dye = not dye
             for pos, _ in board(key=key):
-                if self.offset == 0:
-                    _dye = dye ^ ((pos.x // 2 + pos.y // 2) % 2 > 0)
-                elif self.offset == 1:
-                    _dye = dye ^ (((pos.x + 1) // 2 + pos.y // 2) % 2 > 0)
-                elif self.offset == 2:
-                    _dye = dye ^ ((pos.x // 2 + (pos.y + 1) // 2) % 2 > 0)
-                else:
-                    _dye = dye ^ (((pos.x + 1)//2 + (pos.y + 1)//2) % 2 > 0)
+                # if self.offset == 0:
+                #     _dye = dye ^ ((pos.x // 2 + pos.y // 2) % 2 > 0)
+                # elif self.offset == 1:
+                #     _dye = dye ^ (((pos.x + 1) // 2 + pos.y // 2) % 2 > 0)
+                # elif self.offset == 2:
+                #     _dye = dye ^ ((pos.x // 2 + (pos.y + 1) // 2) % 2 > 0)
+                # else:
+                #     _dye = dye ^ (((pos.x + 1)//2 + (pos.y + 1)//2) % 2 > 0)
+                _dye = dye ^ (((pos.x + self.dx) // self.size + (pos.y + self.dy) // self.size) % 2 > 0)
                 board.set_dyed(pos, _dye)
