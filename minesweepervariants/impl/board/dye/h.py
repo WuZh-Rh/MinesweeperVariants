8c00abf3fca77edb391b6e39e4f40ad0d1807329
mojimoon <cryovit@outlook.com>
2025-08-28 17:32:47 +0800

    rename @h to @hf and @ho to @h

--- minesweepervariants/impl/board/dye/h.py
+++ minesweepervariants/impl/board/dye/h.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-#
-# @Time    : 2025/07/28 03:26
-# @Author  : Wu_RH
-# @FileName: h.py
-
-from ....utils.tool import get_random
 from . import AbstractDye
 
-
-class DyeC(AbstractDye):
+class DyeH(AbstractDye):
     name = "h"
-    __doc__ = "随机染色(50%)"
+    __doc__ = "水平染色"
 
     def __init__(self, args):
         if args:
-            self.percentage = int(args) / 100
+            self.offset = 1
         else:
-            self.percentage = 0.5
+            self.offset = 0
 
     def dye(self, board):
-        positions = [pos for pos, _ in board()]
-        positions = get_random().sample(positions, round(len(positions) * self.percentage))
-        for pos in positions:
-            board.set_dyed(pos, True)
+        dye = True
+        for key in board.get_interactive_keys():
+            dye = not dye
+            for pos, _ in board(key=key):
+                _dye = dye ^ ((pos.x + self.offset) % 2 > 0)
+                board.set_dyed(pos, _dye)
